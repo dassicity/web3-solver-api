65cad0d9f2494fcc41361d2493864b1a1ef884df
Soumyanil Das <dassic@outlook.com>
2023-10-12 05:28:03 +0530

    added domain name routes

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 
+const nameRoutes = require("./routes/resolveNameRoutes");
+const addressRoutes = require("./routes/resolveAddressRoutes");
+
 const app = express();
 
+app.use(express.json());
+
 const PORT = 1331;
+
+app.use("/api/name", nameRoutes);
+app.use("api/address", addressRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`);
---
+++ routes/resolveNameRoutes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const { resolveEthInfo } = require("../controllers/ethController");
+const { resolveSolInfo } = require("../controllers/solController");
+const { resolveBscInfo } = require("../controllers/bscController");
+
+const router = express.Router();
+
+router.get("/resolve/:domain", async (req, res, next) => {
+    let domainName = req.params.domain;
+
+    let response;
+
+    if (domainName.endsWith(".eth")) {
+        response = await resolveEthInfo(domainName);
+    }
+    else if (domainName.endsWith(".sol")) {
+        response = await resolveSolInfo(domainName);
+    }
+    else if (domainName.endsWith(".bnb")) {
+        response = await resolveBscInfo(domainName);
+    }
+
+    if (response) {
+        res.status(201).json({ response });
+    }
+    else {
+        res.status(404).json({ response });
+    }
+});
+
+module.exports = router;
